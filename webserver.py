cedb5381098106e34997be55c673367808125c81
Ryan Connor <ryanwc13@gmail.com>
2016-03-24 19:13:49 +0700

    allow user to input data

--- webserver.py
+++ webserver.py
@@ -1,7 +1,10 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+# import common gateway interface
+import cgi
 
 # handler -- specify what to do based on type of request
 class webserverHandler(BaseHTTPRequestHandler):
+
     def do_GET(self):
         try:
             if self.path.endswith("/hello"):
@@ -10,7 +13,13 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>Hello!</body></html>"
+                output += "<html><body>"
+                output += "Hello!"
+                output += "<form method='POST' enctype='multipart/form-data' "\
+                          "action='/hello'><h2>What would you like me to say?"\
+                          "</h2><input name='message' type='text' ><input "\
+                          "type='submit' value='Submit'></form>"
+                output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
@@ -20,12 +29,46 @@ class webserverHandler(BaseHTTPRequestHandler):
                 self.end_headers()
 
                 output = ""
-                output += "<html><body>&#161Hola! <a href = '/hello' "\
-                          ">Back to Hello</a></body></html>"
+                output += "<html><body>"
+                output += "&#161Hola! <a href = '/hello' "\
+                          ">Back to Hello</a>"
+                output += "<form method='POST' enctype='multipart/form-data' "\
+                          "action='/hello'><h2>What would you like me to say?"\
+                          "</h2><input name='message' type='text' ><input "\
+                          "type='submit' value='Submit'></form>"
+                output += "</body></html>"
                 self.wfile.write(output)
                 print output
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.end_headers()
+
+            # parse an html header into a main value and a dictionary of params
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+
+            # check if the data is form data, do the post if so
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+
+                output = ""
+                output += "<html><body>"
+                output += " <h2> Okay, how about this: </h2>"
+                output += "<h1> %s </h1>" % messagecontent[0]
+                output += "<form method='POST' enctype='multipart/form-data' "\
+                          "action='/hello'><h2>What would you like me to say?"\
+                          "</h2><input name='message' type='text' ><input "\
+                          "type='submit' value='Submit'></form>"
+                output += "</body></html>"
+
+                self.wfile.write(output)
+                print output
+            except:
+                pass
 
 # instantiate server and specify port
 def main():
