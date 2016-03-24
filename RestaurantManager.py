a6dcced0457fc27ce80c95f0fc5df5a82bbebe0e
Ryan Connor <ryanwc13@gmail.com>
2016-03-24 18:19:26 +0700

    basic manager for querying data

---
+++ RestaurantManager.py
@@ -0,0 +1,27 @@
+from database_setup.py import Base, Restaruant, MenuItem
+
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+
+from database_setup.py import Base, Restaurant, MenuItem
+
+
+def getRestaurantDBSession():
+    """Return an interactive session with the puppy adoption database
+    """
+    engine = create_engine('sqlite:///restaurantmenu.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    return session
+
+def getRestaurants():
+    """Return a list of all restaurants ordered by ID
+    """
+    session = getRestaurantDBSession()
+    
+    restaurants = session.query(Restaurant).order_by(Restaurant.id)
+    
+    session.close()
+    return restaurants
+
