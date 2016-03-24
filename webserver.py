05a3439b571c378a20b026d1cd5c43dcefa2339c
Ryan Connor <ryanwc13@gmail.com>
2016-03-25 02:07:07 +0700

    add new restaurant to DB thru GUI

--- webserver.py
+++ webserver.py
@@ -1,7 +1,12 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
 # import common gateway interface
 import cgi
+
 import RestaurantManager
+from database_setup import Base, Restaurant, MenuItem
+
+import bleach
 
 # handler -- specify what to do based on type of request
 class webserverHandler(BaseHTTPRequestHandler):
@@ -22,25 +27,49 @@ class webserverHandler(BaseHTTPRequestHandler):
                 for restaurant in restaurants:
                     output += "<p>"
                     output += restaurant.name+" (ID "+str(restaurant.id)+")"
+                    output += "<br>"
+                    output += "<a href=/edit>Edit</a>"
+                    output += " | "
+                    output += "<a href=/delete>Delete</a>"
                     output += "</p>"
                 
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
-            if self.path.endswith("/hola"):
+            if self.path.endswith("/newrestaurant"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
 
+                restaurants = RestaurantManager.getRestaurants()
+                popCuisines = RestaurantManager.getPopularCuisines()
+
                 output = ""
                 output += "<html><body>"
-                output += "&#161Hola! <a href = '/hello' "\
-                          ">Back to Hello</a>"
-                output += "<form method='POST' enctype='multipart/form-data' "\
-                          "action='/hello'><h2>What would you like me to say?"\
-                          "</h2><input name='message' type='text' ><input "\
-                          "type='submit' value='Submit'></form>"
+                output += "<h1>Enter a New Restaurant into the Database</h1>"
+                output += "<form method='POST' enctype='multipart/form-data'>"
+                output += "<h3>Name of Restaurant</h3>"
+                output += "Enter the name of the resaurant here:<br>"
+                output += "<input name='name' type='text' size='50'><br>"
+                
+                output += "<h3>Type of Cuisine</h3>"
+                output += "<input name='commOrCust' type='radio' value='common'>"
+                output += " Use common cuisine from the provided list<br>"
+                output += "<input name='commOrCust' type='radio' value='custom'>"
+                output += " Use custom cuisine typed into the box below<br>"
+                output += "<br>"
+                output += "Common cuisines (based on existing restaurants):<br>"
+
+                for cuisine in popCuisines:
+                    output += "<input type='radio' name='cuisine' value='"
+                    output += cuisine.foodType+"'> "+cuisine.foodType+"<br>"
+                
+                output += "<br>Enter custom cuisine here:<br>"
+                output += "<input name='custCuisine' type='text' size='50'><br>"
+                
+                output += "<br><input type='submit' value='Submit Restaurant'>"
+                output += "</form>"
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
@@ -53,23 +82,40 @@ class webserverHandler(BaseHTTPRequestHandler):
             self.end_headers()
 
             # parse an html header into a main value and a dictionary of params
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            ctype, pdict = cgi.parse_header(self.headers.\
+                                            getheader('content-type'))
 
             # check if the data is form data, do the post if so
             if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
+                formInfo = cgi.parse_multipart(self.rfile, pdict)
+
+                # use bleach on user provided text to defend SQL injection
+                name = bleach.clean(formInfo['name'][0])
+                cuisine = ""
+                
+                if formInfo['commOrCust'][0] == 'custom':
+                    cuisine = bleach.clean(formInfo['custCuisine'][0])
+                else:
+                    cuisine = formInfo['cuisine'][0]
+
+                newRestaurant = Restaurant(name=name,foodType=cuisine)
+                session = RestaurantManager.getRestaurantDBSession()
+                session.add(newRestaurant)
+                session.commit()
+                session.close()
+                
+                message = ""
+                message += "Added %s " % name
+                message += "with cuisine type %s" % cuisine
+                message += " to the database"
 
                 output = ""
                 output += "<html><body>"
-                output += " <h2> Okay, how about this: </h2>"
-                output += "<h1> %s </h1>" % messagecontent[0]
-                output += "<form method='POST' enctype='multipart/form-data' "\
-                          "action='/hello'><h2>What would you like me to say?"\
-                          "</h2><input name='message' type='text' ><input "\
-                          "type='submit' value='Submit'></form>"
-                output += "</body></html>"
-
+                output += "<h1>"+message+"</h1>"
+                output += "<h3>To continue, refresh the page "
+                output += "or navigate to another page"
+                output += "</body><html>"
+                
                 self.wfile.write(output)
                 print output
         except:
