590b83054d5a39e23bd3d0e0e02b20edb0afeb86
Ryan Connor <ryanwc13@gmail.com>
2016-03-25 14:22:38 +0700

    edit a restaurant thru GUI

--- webserver.py
+++ webserver.py
@@ -7,13 +7,14 @@ import RestaurantManager
 from database_setup import Base, Restaurant, MenuItem
 
 import bleach
+import re
 
 # handler -- specify what to do based on type of request
 class webserverHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/restaurants"):
+            if self.path.endswith("/restList"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
@@ -28,16 +29,20 @@ class webserverHandler(BaseHTTPRequestHandler):
                     output += "<p>"
                     output += restaurant.name+" (ID "+str(restaurant.id)+")"
                     output += "<br>"
-                    output += "<a href=/edit>Edit</a>"
+                    output += "<a href=/"
+                    output += str(restaurant.id)
+                    output += "restEdit>Edit</a>"
                     output += " | "
-                    output += "<a href=/delete>Delete</a>"
+                    output += "<a href=/"
+                    output += str(restaurant.id)
+                    output += "restDelete>Delete</a>"
                     output += "</p>"
                 
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
                 return
-            if self.path.endswith("/newrestaurant"):
+            if self.path.endswith("/restAdd"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
@@ -48,7 +53,8 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><body>"
                 output += "<h1>Enter a New Restaurant into the Database</h1>"
-                output += "<form method='POST' enctype='multipart/form-data'>"
+                output += "<form name='addRest' method='POST' "
+                output += "enctype='multipart/form-data'>"
                 output += "<h3>Name of Restaurant</h3>"
                 output += "Enter the name of the resaurant here:<br>"
                 output += "<input name='name' type='text' size='50'><br>"
@@ -73,6 +79,51 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output += "</body></html>"
                 self.wfile.write(output)
                 print output
+            if self.path.endswith("restEdit"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                localPath = re.search('/[0-9]+restEdit',\
+                                      str(self.path)).group(0)
+                rest_id = re.search('[0-9]+',localPath).group(0)
+                restaurant = RestaurantManager.getRestaurant(int(rest_id))
+                
+                popCuisines = RestaurantManager.getPopularCuisines()
+
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Edit Restaurant %s</h1>" % rest_id
+                output += "<form name='editRest' method='POST' "
+                output += "enctype='multipart/form-data'>"
+                output += "<h3>Edit Name of Restaurant</h3>"
+                output += "Current name: <em>" + restaurant.name
+                output += "</em><br><br>"
+                output += "Enter a new name for the resaurant here:<br>"
+                output += "<input name='name' type='text' size='50'><br>"
+                
+                output += "<h3>Edit Type of Cuisine</h3>"
+                output += "Current cuisine: <em>" + restaurant.foodType
+                output += "</em><br><br>"
+                output += "<input name='commOrCust' type='radio' value='common'>"
+                output += " Use common cuisine from the provided list<br>"
+                output += "<input name='commOrCust' type='radio' value='custom'>"
+                output += " Use custom cuisine typed into the box below<br>"
+                output += "<br>"
+                output += "Common cuisines (based on existing restaurants):<br>"
+
+                for cuisine in popCuisines:
+                    output += "<input type='radio' name='cuisine' value='"
+                    output += cuisine.foodType+"'> "+cuisine.foodType+"<br>"
+                
+                output += "<br>Enter custom cuisine here:<br>"
+                output += "<input name='custCuisine' type='text' size='50'><br>"
+                
+                output += "<br><input type='submit' value='Edit Restaurant'>"
+                output += "</form>"
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
@@ -89,6 +140,25 @@ class webserverHandler(BaseHTTPRequestHandler):
             if ctype == 'multipart/form-data':
                 formInfo = cgi.parse_multipart(self.rfile, pdict)
 
+                localPath = ""
+                rest_id = ""
+                restaurant = []
+
+                if self.path.endswith("restEdit"):
+                    formInfo['form'] = 'restEdit'
+                    localPath = re.search('/[0-9]+restEdit',\
+                                          str(self.path)).group(0)
+                    rest_id = re.search('[0-9]+',localPath).group(0)
+                    restaurant = RestaurantManager.getRestaurant(int(rest_id))
+                elif self.path.endswith("restDelete"):
+                    formInfo['form'] = 'restDelete'
+                    localPath = re.search('/[0-9]+restDelete',\
+                                          str(self.path)).group(0)
+                    rest_id = re.search('[0-9]+',localPath).group(0)
+                    restaurant = RestaurantManager.getRestaurant(int(rest_id))
+                elif self.path.endswith("restAdd"):
+                    formInfo['form'] = 'restAdd'
+
                 # use bleach on user provided text to defend SQL injection
                 name = bleach.clean(formInfo['name'][0])
                 cuisine = ""
@@ -98,16 +168,54 @@ class webserverHandler(BaseHTTPRequestHandler):
                 else:
                     cuisine = formInfo['cuisine'][0]
 
-                newRestaurant = Restaurant(name=name,foodType=cuisine)
                 session = RestaurantManager.getRestaurantDBSession()
-                session.add(newRestaurant)
+
+                message = ""
+                
+                if formInfo['form'] == 'restAdd':
+                    if (len(nam) > 0 and len(cuisine) > 0):
+                        newRestaurant = Restaurant(name=name,foodType=cuisine)
+                        session.add(newRestaurant)
+                        message += "Added %s " % name
+                        message += "with cuisine type %s" % cuisine
+                        message += " to the database"
+                    else:
+                        message += "Nothing added because name or cuisine empty"
+                elif formInfo['form'] == 'restEdit':
+
+                    message += "Made these changes to restaurant %s: " % rest_id
+                    if (name == restaurant.name and
+                        cuisine == restaurant.foodType):
+                        message += "<br>None because name and cuisine were not "
+                        message += "different"
+                    elif (len(name) == 0 and len(cuisine) == 0):
+                        message += "<br>None because name and cuisine fields "
+                        message += "were empty"
+                    else:
+                        if name != restaurant.name:
+                            print "new name"
+                            session.query(Restaurant).\
+                            filter(Restaurant.id==rest_id).\
+                            update({'name':name})
+                            message += "<br>Name from %s " % restaurant.name
+                            message += "to %s" % name
+                            print "done new name"
+                        if cuisine != restaurant.foodType:
+                            session.query(Restaurant).\
+                            filter(Restaurant.id==rest_id).\
+                            update({'foodType':cuisine})
+                            message += "<br>Cuisine from %s " % restaurant.foodType
+                            message += "to %s" % cuisine
+                elif formInfo['form'] == 'restDelete':
+                    session.query(Restaurant).\
+                    filter(Restaurant.id==rest_id).\
+                    delete()
+                    message += "Deleted %s " % name
+                    message += "(id %s )" % rest_id
+                    message += " from the database"
+
                 session.commit()
                 session.close()
-                
-                message = ""
-                message += "Added %s " % name
-                message += "with cuisine type %s" % cuisine
-                message += " to the database"
 
                 output = ""
                 output += "<html><body>"
