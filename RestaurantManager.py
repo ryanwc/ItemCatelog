5410cbf754503b0c2534f6d3c1695956ef92f86d
Ryan Connor <ryanwc13@gmail.com>
2016-05-07 12:28:34 +0700

    manage editing of restaurants, cuisines, restaurant menu items, and base menu items

--- RestaurantManager.py
+++ RestaurantManager.py
@@ -127,10 +127,11 @@ def getRestaurantMenuItems():
     """
     session = getRestaurantDBSession()
 
-    menuItems = session.query(MenuItem).order_by(MenuItem.restaurant_id).all()
+    restaurantMenuItems = session.query(RestaurantMenuItem).\
+                          order_by(RestaurantMenuItem.restaurant_id).all()
 
     session.close()
-    return menuItems
+    return restaurantMenuItems
 
 def getRestaurantMenuItems(rest_id):
     """Return a list of all menu items for a specific restaurant
@@ -140,11 +141,25 @@ def getRestaurantMenuItems(rest_id):
     """
     session = getRestaurantDBSession()
 
-    menuItems = session.query(RestaurantMenuItem).\
-                filter_by(restaurant_id=rest_id).all()
+    restaurantMenuItems = session.query(RestaurantMenuItem).\
+                          filter_by(restaurant_id=rest_id).all()
 
     session.close()
-    return menuItems
+    return restaurantMenuItems
+
+def getRestaurantMenuItem(restaurantMenuItem_id):
+    """Return a restaurant menu item with the given it
+
+    Args:
+        restaurantMenuItem_id: the id of the restaurant menu item to get
+    """
+    session = getRestaurantDBSession()
+
+    restaurantMenuItem = session.query(RestaurantMenuItem).\
+                         filter_by(id=restaurantMenuItem_id).one()
+
+    session.close()
+    return restaurantMenuItem
 
 def getPopularCuisines():
     """Return a list of all cuisines offered by at least three restaurants
@@ -178,15 +193,156 @@ def getCuisine(cuisine_id=None, name=None):
     """Return the cuisine with the given id
 
     Args:
-        cuisine_id: the id of the cuisine to get
+        cuisine_id: the id of the cuisine to get.
+            Pass None to get by name instead.
+        name: the name of the cuisine to get.  Pass None to get by id instead.
     """
     session = getRestaurantDBSession()
 
     if cuisine_id is not None:
         cuisine  = session.query(Cuisine).filter_by(id=cuisine_id).one()
-    else:
+    else if name is not None:
         cuisine = session.query(Cuisine).filter_by(name=name).one()
 
     session.close()
     return cuisine
 
+def editRestaurant(restaurantMenuItem_id, newName=None, newCuisine_id=None):
+    """Edit a restaurant
+
+    Args:
+        restaurant_id: the id of the restaurant to edit
+        newName: a new name of the restaurant to edit.  Pass none to leave
+            the name unchanged.
+        newCuisine_id: the id of the restaurant's new cuisine.
+            Pass None to leave the cuisine unchanged.
+    """
+    session = getRestaurantDBSession()
+
+    if newName is not None:
+        session.query(Restaurant).filter_by(id=restaurant_id).\
+            update({'name':newName})
+
+    if newCuisine_id is not None:
+        session.query(Restaurant).filter_by(id=restaurant_id).\
+            update({'cuisine_id':newCuisine_id})
+
+    session.commit()
+    session.close()
+
+def editRestaurantMenuItem(restaurantMenuItem_id, newName=None, 
+                           newDescription=None, newPrice=None,
+                           newCourse=None, newRestaurant_id=None,
+                           newBaseMenuItem_id=None):
+    """Edit a restaurant menu item
+
+    Args:
+        restaurantMenuItem_id: the id of the restaurant menu item to edit
+        newName: a new name for the restaurant menu item.
+            Pass none to leave the name unchanged.
+        newDescription: a new description for the restaurant menu item.
+            Pass None to leave the description unchanged.
+        newPrice: a new price for the restaurant menu item.
+            Pass None to leave the price unchanged.
+        newCourse: the restaurant menu item's new course.
+            Pass None to leave the course unchanged.
+        newRestaurant_id: the id of the restaurant menu item's new restaurant.
+            Pass None to leave the restaurant unchanged.
+        newBaseMenuItem_id: the id of the restaurant menu item's new
+            base menu item. Pass None to leave the base menu item unchanged.
+    """
+    session = getRestaurantDBSession()
+
+    if newName is not None:
+        session.query(RestaurantMenuItem).\
+            filter_by(id=restaurantMenuItem_id).\
+            update({'name':newName})
+
+    if newDescription is not None:
+        session.query(RestaurantMenuItem).\
+            filter_by(id=restaurantMenuItem_id).\
+            update({'description':newDescription})
+
+    if newPrice is not None:
+        session.query(RestaurantMenuItem).\
+            filter_by(id=restaurantMenuItem_id).\
+            update({'price':newPrice})
+
+    if newCourse is not None:
+        session.query(RestaurantMenuItem).\
+            filter_by(id=restaurantMenuItem_id).\
+            update({'course':newCourse})
+
+    if newRestaurant_id is not None:
+        session.query(RestaurantMenuItem).\
+            filter_by(id=restaurantMenuItem_id).\
+            update({'restaurant_id':newRestaurant_id})
+
+    if newBaseMenuItem_id is not None:
+        session.query(RestaurantMenuItem).\
+            filter_by(id=restaurantMenuItem_id).\
+            update({'baseMenuItem_id':newBaseMenuItem_id})
+
+    session.commit()
+    session.close()
+
+def editCuisine(cuisine_id, newName=None):
+    """Edit a cuisine
+
+    Args:
+        cuisine_id: the id of the cuisine to edit
+        newName: the new name of the cuisine to edit.  Pass none to leave
+            the name unchanged.
+    """
+    session = getRestaurantDBSession()
+
+    if newName is not None:
+        session.query(Cuisine).filter_by(id=cuisine_id).\
+            update({'name':newName})
+
+    session.commit()
+    session.close()
+
+def editBaseMenuItem(baseMenuItem_id, newName=None, 
+                     newDescription=None, newPrice=None,
+                     newCourse=None, newCuisine_id=None):
+    """Edit a base menu item
+
+    Args:
+        baseMenuItem_id: the id of the base menu item to edit
+        newName: a new name for the base menu item.
+            Pass none to leave the name unchanged.
+        newDescription: a new description for the base menu item.
+            Pass None to leave the description unchanged.
+        newPrice: a new price for the base menu item.
+            Pass None to leave the price unchanged.
+        newCourse: the base menu item's new course.
+            Pass None to leave the course unchanged.
+        newCuisine_id: the id of the base menu item's new cuisine.
+            Pass None to leave the cuisine unchanged.
+    """
+    session = getRestaurantDBSession()
+
+    if newName is not None:
+        session.query(BaseMenuItem).filter_by(id=baseMenuItem_id).\
+            update({'name':newName})
+
+    if newDescription is not None:
+        session.query(BaseMenuItem).\
+            filter_by(id=baseMenuItem_id).\
+            update({'description':newDescription})
+
+    if newPrice is not None:
+        session.query(BaseMenuItem).filter_by(id=baseMenuItem_id).\
+            update({'price':newPrice})
+
+    if newCourse is not None:
+        session.query(BaseMenuItem).filter_by(id=baseMenuItem_id).\
+            update({'course':newCourse})
+
+    if newCuisine_id is not None:
+        session.query(BaseMenuItem).filter_by(id=baseMenuItem_id).\
+            update({'cuisine_id':newCuisine_id})
+
+    session.commit()
+    session.close()
